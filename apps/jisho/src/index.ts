ddaba63a9c924fb4bc568db8f96d6cdd8fb5bfbb
TulioG_Projects <tuliog.projects@gmail.com>
2026-04-12 02:49:29 +0800

    WIP

--- apps/jisho/src/index.ts
+++ apps/jisho/src/index.ts
@@ -1,15 +1,21 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Hono } from "hono";
 
-const app = new Hono()
+const db = drizzle(process.env.DATABASE_URL!);
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
