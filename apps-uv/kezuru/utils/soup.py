7441940d9002d5b0e3a5c095c087d3e67b32feda
TulioG_Projects <tuliog.projects@gmail.com>
2026-08-22 23:08:21 +0800

    bugfix: fix unexpected behavior of vocab scrape after moving from html.parser to html5lib

--- apps-uv/kezuru/utils/soup.py
+++ apps-uv/kezuru/utils/soup.py
@@ -20,6 +20,12 @@ def filter_from_page_element_list(
     return [item for item in list_var if item != pattern_to_use]
 
 
+def filter_navigablestring_from_element_list(
+    list_var: list[PageElement],
+) -> list[PageElement]:
+    return [item for item in list_var if not isinstance(item, NavigableString)]
+
+
 def reconstruct_stripped_strings(stripped_strings: Iterator[str]) -> str:
     reconstructed = " ".join(stripped_strings)
     return reconstructed
